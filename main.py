cb83deb33977cf16721b3017584918ce7a2a5c4f
joao saad <joaofernandessaad@gmail.com>
2026-05-22 18:17:56 -0300

    commit 3

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import pygame
+from config import LARGURA, ALTURA, FPS, TELA_INICIO, SAIR
 
 pygame.init()
 pygame.mixer.init()
 
-LARGURA, ALTURA = 600, 600
 window = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption('Smili, o rato')
 
-clock = pygame.time.Clock()
-rodando = True
-while rodando:
-    clock.tick(30)
+estado = TELA_INICIO
+clock  = pygame.time.Clock()
+
+
+while estado != SAIR:
+    clock.tick(FPS)
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
-            rodando = False
+            estado = SAIR
+        if ev.type == pygame.KEYDOWN:
+            if ev.key == pygame.K_ESCAPE:
+                estado = SAIR
+
 
 pygame.quit()
