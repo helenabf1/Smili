636e87e2230086bb6eb4cd59e6a230782b69186b
joao saad <joaofernandessaad@gmail.com>
2026-05-23 11:38:35 -0300

    comit 5

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
-from config import LARGURA, ALTURA, FPS, TELA_INICIO, SAIR
+from config import LARGURA, ALTURA, FPS, TELA_INICIO, TELA_JOGO, SAIR
+from assets import carregar_assets
+from telas import tela_inicio
 
 pygame.init()
 pygame.mixer.init()
@@ -7,18 +9,14 @@ pygame.mixer.init()
 window = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption('Smili, o rato')
 
+assets = carregar_assets()
 estado = TELA_INICIO
-clock  = pygame.time.Clock()
-
 
 while estado != SAIR:
-    clock.tick(FPS)
-    for ev in pygame.event.get():
-        if ev.type == pygame.QUIT:
-            estado = SAIR
-        if ev.type == pygame.KEYDOWN:
-            if ev.key == pygame.K_ESCAPE:
-                estado = SAIR
+    if estado == TELA_INICIO:
+        estado = tela_inicio(window, assets)
+    else:
+        estado = SAIR
 
 
 pygame.quit()
